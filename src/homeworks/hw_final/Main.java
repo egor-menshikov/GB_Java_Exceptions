f2feff9c4baea86ced690bb387568c6c6f1b78b2
Egor Menshikov <r3born.fs@gmail.com>
2023-09-06 11:23:30 +0400

    hw_final prepareUserData done

--- src/homeworks/hw_final/Main.java
+++ src/homeworks/hw_final/Main.java
@@ -1,6 +1,5 @@
 package homeworks.hw_final;
 
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -8,40 +7,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        try {
-//            String[] text = getUserData();
-//        } catch (ArraySizeException e) {
-//            System.out.println(e.getMessage());
-//        }
-        String d = "21.02.2023";
-        String e = "19.02-2024";
-        String f = "21.02.2023";
-        LocalDate date = LocalDate.parse(d, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-        System.out.println(date);
-        LocalDate date1 = LocalDate.parse(e, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-        LocalDate date2 = LocalDate.parse(f, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-        System.out.println(date1.compareTo(date));
-    }
-
-    public static String prepareUserData(String[] text) {
         try {
-            checkNames(text[0], text[1], text[2]);
-            checkDate(text[3]);
-        } catch (WrongDataException e) {
+            String[] text = getUserData();
+            String s = prepareUserData(text);
+            System.out.println(s);
+        } catch (ArraySizeException | WrongDataException e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    public static String prepareUserData(String[] text) throws WrongDataException {
+        checkNames(text[0], text[1], text[2]);
+        checkDate(text[3]);
+        checkPhone(text[4]);
+        checkGender(text[5]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length; i++) {
+            if (i < text.length - 1)
+                sb.append(text[i]).append(" ");
+            else
+                sb.append(text[i]).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static void checkGender(String s) throws WrongDataException {
+        if (s.equalsIgnoreCase("m") || s.equalsIgnoreCase("f"))
+            return;
+        throw new WrongDataException("Пол указан неверно");
+    }
+
+    private static void checkPhone(String s) throws WrongDataException {
+        if (s.length() != 11 || !s.matches("^[0-9]*$"))
+            throw new WrongDataException("Телефон указан неверно.");
     }
 
     private static void checkDate(String s) throws WrongDataException {
-        LocalDate today = LocalDate.of(2023,9, 5);
+        LocalDate today = LocalDate.of(2023, 9, 5);
         try {
             LocalDate date = LocalDate.parse(s, DateTimeFormatter.ofPattern("dd.MM.uuuu"));
             if (date.isAfter(today)) {
                 throw new WrongDataException("Дата указана неверно.");
             }
         } catch (DateTimeParseException e) {
-            System.out.println("Дата введена в неверном формате.");
+            throw new WrongDataException("Дата была указана в неверном формате.");
         }
     }
 
@@ -53,8 +62,6 @@ public class Main {
         if (!s3.matches("^[a-zA-Zа-яА-Я]*$"))
             throw new WrongDataException("Отчество введено неверно.");
     }
-
-
 
 
     public static String[] getUserData() throws ArraySizeException {
